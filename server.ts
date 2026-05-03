1d473c82d8af26d7e729efc9430ef3f53d690f32
Krushna0506 <mankarikrushna@gmail.com>
2026-05-03 15:03:45 +0530

    enable cors

--- server.ts
+++ server.ts
@@ -8,6 +8,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://cipla-emeu-face-recognition.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(cors({ origin: '*' }));
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
